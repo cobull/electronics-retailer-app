3a717f66057d5d5c4b45a3a6e965ea8496c4f521
Cody B <67527461+cobull@users.noreply.github.com>
2025-02-04 16:26:49 -0600

    Initial commit

---
+++ src/main/java/com/codyb/electronicsretailerapp/ElectronicsRetailerAppApplication.java
@@ -0,0 +1,13 @@
+package com.codyb.electronicsretailerapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectronicsRetailerAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElectronicsRetailerAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codyb/electronicsretailerapp/ElectronicsRetailerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codyb.electronicsretailerapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElectronicsRetailerAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
